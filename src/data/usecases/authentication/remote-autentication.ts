442886937e4b425362f74342816b1ad520472610
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-04-04 12:21:27 -0300

    feat: add RemoteAuthentication with HttpPostClient

---
+++ src/data/usecases/authentication/remote-autentication.ts
@@ -0,0 +1,12 @@
+import { type HttpPostClient } from 'data/protocols/http/postClient'
+
+export class RemoteAuthentication {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpPostClient
+  ) {}
+
+  async auth (): Promise<void> {
+    await this.httpClient.post(this.url)
+  }
+}
