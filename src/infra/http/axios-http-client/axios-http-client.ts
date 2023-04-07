2d9cd5af048190e5eb4b5ffe293334e2e08365da
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-04-07 14:42:17 -0300

    feat: ensure AxiosHttpClient calls post with correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -2,7 +2,7 @@ import { type HttpPostClientInput } from '@/data/protocols/http'
 import axios from 'axios'
 
 export class AxiosHttpClient {
-  async post ({ url }: HttpPostClientInput<any>): Promise<void> {
-    await axios.post(url)
+  async post ({ url, body }: HttpPostClientInput<any>): Promise<void> {
+    await axios.post(url, body)
   }
 }
