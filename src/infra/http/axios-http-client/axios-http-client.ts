92d9843ec097c8c4da7408c900b011281fa4de63
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-04-07 15:53:41 -0300

    feat: ensure AxiosHttpClient returns correct status and body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,12 @@
-import { type HttpPostClientInput } from '@/data/protocols/http'
+import { type HttpResponse, type HttpPostClientInput, type HttpPostClient } from '@/data/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async post ({ url, body }: HttpPostClientInput<any>): Promise<void> {
-    await axios.post(url, body)
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post ({ url, body }: HttpPostClientInput<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(url, body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
