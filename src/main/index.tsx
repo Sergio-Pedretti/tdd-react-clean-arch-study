fb69920ac52e6369ff9eb304d463a1cf4a4c47aa
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-04-10 13:20:29 -0300

    chore: finish vite config with react

---
+++ src/main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = document.getElementById('root') as HTMLElement
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <div >
+        HELLO GUYS
+    </div>
+  </React.StrictMode>
+)
