b74802217796dc2ddfeaeacecdb5392a250d4083
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-04-18 17:47:30 -0300

    feat: ensure login will not show spin and error on start

--- src/presentations/components/form-status/form-status.tsx
+++ src/presentations/components/form-status/form-status.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import content from './form-status.module.scss'
 import { Spinner } from '@/presentations/components'
+import Context from '@/presentations/contexts/form/form-context'
 
 export const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
+
   return (
-    <div className={content.errorWrap}>
-        <Spinner className={content.spinner} />
-        <span className={content.error}>Erro</span>
+    <div data-testid="error-wrap" className={content.errorWrap}>
+        {isLoading && <Spinner className={content.spinner} />}
+        {errorMessage && <span className={content.error}>{errorMessage}</span>}
     </div>
   )
 }
--- src/presentations/pages/login/login.tsx
+++ src/presentations/pages/login/login.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import content from './login-style.module.scss'
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentations/components'
+import Context from '@/presentations/contexts/form/form-context'
+
+type statesProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 
 export const Login: React.FC = () => {
+  const [state] = useState<statesProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <div className={content.login}>
       <LoginHeader />
-      <form className={content.form}>
-        <h2>Login</h2>
-        <Input type='email' name='email' placeholder='Digite seu e-mail' />
-        <Input type='password' name='password' placeholder='Digite sua senha' />
-        <button className={content.submit} type='submit'>Entrar</button>
-        <span className={content.link}>Criar conta</span>
-        <FormStatus />
-      </form>
-        <Footer />
+      <Context.Provider value={state}>
+        <form className={content.form}>
+          <h2>Login</h2>
+          <Input type='email' name='email' placeholder='Digite seu e-mail' />
+          <Input type='password' name='password' placeholder='Digite sua senha' />
+          <button className={content.submit} type='submit'>Entrar</button>
+          <span className={content.link}>Criar conta</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
     </div>
   )
 }
