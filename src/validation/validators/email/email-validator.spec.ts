7b0414c942d634672885b33c47796393ffb36dd5
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-04-26 14:50:50 -0300

    refactor: isolate SUT

--- src/validation/validators/email/email-validator.spec.ts
+++ src/validation/validators/email/email-validator.spec.ts
@@ -2,9 +2,19 @@ import { EmailValidator } from './email-validator'
 import { InvalidFieldError } from '@/validation/errors'
 import { faker } from '@faker-js/faker'
 
+type SutTypes = {
+  sut: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  return {
+    sut: new EmailValidator('email')
+  }
+}
+
 describe('EmailValidator', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidator('email')
+    const { sut } = makeSut()
 
     const error = sut.validate('')
 
@@ -12,7 +22,7 @@ describe('EmailValidator', () => {
   })
 
   it('should return falsy if email is valid', () => {
-    const sut = new EmailValidator('email')
+    const { sut } = makeSut()
 
     const result = sut.validate(faker.internet.email())
 
