26bbfac286d8949f9aef625dc44f07948775fb48
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-04-28 11:30:14 -0300

    feat: ensure AxiosHttpClient return correc status and body on fail

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,9 +1,14 @@
 import { type HttpResponse, type HttpPostClientInput, type HttpPostClient } from '@/data/protocols/http'
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post ({ url, body }: HttpPostClientInput<any>): Promise<HttpResponse<any>> {
-    const httpResponse = await axios.post(url, body)
+    let httpResponse: AxiosResponse<any>
+    try {
+      httpResponse = await axios.post(url, body)
+    } catch (error) {
+      httpResponse = error.response
+    }
     return {
       statusCode: httpResponse.status,
       body: httpResponse.data
