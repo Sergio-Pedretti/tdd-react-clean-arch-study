8fb5b4d9d63f6e2bf60c2b440fa33616cb12861c
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-05-03 09:53:22 -0300

    refactor: isolate SUT creation

--- src/data/usecases/save-access-token/save-access-token.spec.ts
+++ src/data/usecases/save-access-token/save-access-token.spec.ts
@@ -2,10 +2,23 @@ import { SetStorageSpy } from "@/data/test/mock-set-storage"
 import { LocalSaveAccessToken } from "./save-access-token"
 import { faker } from '@faker-js/faker'
 
+type SutTypes = {
+  sut: LocalSaveAccessToken
+  setStorageSpy: SetStorageSpy
+}
+
+const makeSut = ():SutTypes => {
+  const setStorageSpy = new SetStorageSpy()
+  const sut = new LocalSaveAccessToken(setStorageSpy)
+  return {
+    sut,
+    setStorageSpy
+  }
+}
+
 describe('LocalSaveAccessToken', () => {
   it('should call SetStorage with correct method', async () => {
-    const setStorageSpy = new SetStorageSpy()
-    const sut = new LocalSaveAccessToken(setStorageSpy)
+    const {setStorageSpy, sut} = makeSut()
     const accessToken = faker.datatype.uuid()
     await sut.save(accessToken)
     expect(setStorageSpy.key).toBe('accessToken')
