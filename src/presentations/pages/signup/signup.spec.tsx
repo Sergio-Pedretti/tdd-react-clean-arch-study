c522c57929273e5c9be6aae70fb010478b3eb675
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-05-05 14:52:01 -0300

    refactor: isolate testing functions to Helper

--- src/presentations/pages/signup/signup.spec.tsx
+++ src/presentations/pages/signup/signup.spec.tsx
@@ -1,9 +1,10 @@
 import React from "react"
-import { RenderResult, fireEvent, render } from "@testing-library/react"
+import { RenderResult, render } from "@testing-library/react"
 import { SignUp } from "./signup"
 import { MemoryRouter, Routes, Route } from "react-router-dom"
 import { faker } from "@faker-js/faker"
 import { ValidationSpy } from "@/presentations/test"
+import { Helper } from "@/presentations/test"
 
 type SutTypes = {
   sut: RenderResult
@@ -25,27 +26,6 @@ const makeSut = (): SutTypes => {
     }
 }
 
-const testChildCount = (sut: RenderResult, field: string, count:number) => {
-    const element = sut.getByTestId(field)
-    expect(element.childElementCount).toBe(count)
-}
-
-const testButtonIsDisabled = (sut: RenderResult, fieldName:string, isDisabled: boolean) => {
-    const button = sut.getByTestId(fieldName) as HTMLButtonElement
-    expect(button.disabled).toBe(isDisabled)
-}
-
-const testStatusForField = (sut: RenderResult, fieldName:string, validationError: string) => {
-    const fieldStatus = sut.getByTestId(`${fieldName}-status`) 
-    expect(fieldStatus.title).toBe(validationError || "Campo Obrigatório!")
-    expect(fieldStatus.textContent).toBe(validationError ? '🟢': '🔴')
-}
-
-const populateField = (sut: RenderResult, fieldName: string, value = faker.random.word()) => {
-    const field = sut.getByTestId(fieldName)
-    fireEvent.change(field, { target: { value } })
-}
-
 describe('SignUp Component', () => {
     let sut: RenderResult
     let validationSpy: ValidationSpy
@@ -59,19 +39,19 @@ describe('SignUp Component', () => {
   it('should start with initial state', () => {
     const validationError = ''
 
-    testChildCount(sut, 'error-wrap', 0)
-    testButtonIsDisabled(sut, 'submit', true)
-    testStatusForField(sut, 'name', '')
-    testStatusForField(sut, 'email', validationError)
-    testStatusForField(sut, 'password', validationError)
-    testStatusForField(sut, 'passwordConfirmation', validationError)
+    Helper.testChildCount(sut, 'error-wrap', 0)
+    Helper.testButtonIsDisabled(sut, 'submit', true)
+    Helper.testStatusForField(sut, 'name', '')
+    Helper.testStatusForField(sut, 'email', validationError)
+    Helper.testStatusForField(sut, 'password', validationError)
+    Helper.testStatusForField(sut, 'passwordConfirmation', validationError)
   })
 
    it('should show name error if validation fails', () => {
     validationSpy.errorMessage = faker.random.words()
     const name = faker.name.fullName()
 
-    populateField(sut, 'email', name)
-    testStatusForField(sut, 'name', '')
+    Helper.populateField(sut, 'email', name)
+    Helper.testStatusForField(sut, 'name', '')
   })
 })
---
+++ src/presentations/test/form-helper.tsx
@@ -0,0 +1,23 @@
+import { faker } from "@faker-js/faker"
+import { RenderResult, fireEvent } from "@testing-library/react"
+
+export const testChildCount = (sut: RenderResult, field: string, count:number) => {
+    const element = sut.getByTestId(field)
+    expect(element.childElementCount).toBe(count)
+}
+
+export const testButtonIsDisabled = (sut: RenderResult, fieldName:string, isDisabled: boolean) => {
+    const button = sut.getByTestId(fieldName) as HTMLButtonElement
+    expect(button.disabled).toBe(isDisabled)
+}
+
+export const testStatusForField = (sut: RenderResult, fieldName:string, validationError: string) => {
+    const fieldStatus = sut.getByTestId(`${fieldName}-status`) 
+    expect(fieldStatus.title).toBe(validationError || "Campo Obrigatório!")
+    expect(fieldStatus.textContent).toBe(validationError ? '🟢': '🔴')
+}
+
+export const populateField = (sut: RenderResult, fieldName: string, value = faker.random.word()) => {
+    const field = sut.getByTestId(fieldName)
+    fireEvent.change(field, { target: { value } })
+}
