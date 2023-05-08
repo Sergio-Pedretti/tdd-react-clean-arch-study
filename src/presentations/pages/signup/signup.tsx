0440113fceed971555680ed957fd72e05e4a4b48
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-05-08 14:15:20 -0300

    feat: ensure SignUp show spinner on submit

--- src/presentations/pages/signup/signup.tsx
+++ src/presentations/pages/signup/signup.tsx
@@ -10,7 +10,7 @@ type Props = {
 }
 
 const SignUpConsumer: React.FC<Props> = ({ validation }:Props) => {
-  const { setErrorState, errorState, signup } = useLogin()
+  const { setErrorState, errorState, signup, state, setState } = useLogin()
 
   const disableButton = !!errorState.nameError || !!errorState.emailError || !!errorState.passwordError || !!errorState.passwordConfirmationError
 
@@ -49,16 +49,26 @@ const SignUpConsumer: React.FC<Props> = ({ validation }:Props) => {
 
     validation?.validate('passwordConfirmation', signup.passwordConfirmation)
   }, [signup.passwordConfirmation])
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault()
+      if (state.isLoading || errorState.emailError || errorState.passwordError) {
+        return
+      }
+      setState({
+        isLoading: true
+      })
+  }
   
   return (
     <div className={content.signup}>
       <LoginHeader />
-        <form className={content.form}>
+        <form data-testid='form' className={content.form} onSubmit={handleSubmit}>
           <h2>Sign up</h2>
-          <Input data-testid='name' type='text' name='name' placeholder='Digite seu nome' />
-          <Input data-testid='email' type='email' name='email' placeholder='Digite seu e-mail' />
-          <Input data-testid='password' type='password' name='password' placeholder='Digite sua senha' />
-          <Input data-testid='passwordConfirmation' type='password' name='passwordConfirmation' placeholder='Confirme sua senha' />
+          <Input type='text' name='name' placeholder='Digite seu nome' />
+          <Input type='email' name='email' placeholder='Digite seu e-mail' />
+          <Input type='password' name='password' placeholder='Digite sua senha' />
+          <Input type='password' name='passwordConfirmation' placeholder='Confirme sua senha' />
           <button data-testid='submit' disabled={disableButton} className={content.submit} type='submit'>Entrar</button>
           <Link to='/signup' className={content.link}>Voltar Para Login</Link>
           <FormStatus />
